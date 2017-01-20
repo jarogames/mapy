bf711d4cf80347b4028c9cdb243a1389ecdb824a
Jaromrax <jaromrax@gmail.com>
2017-01-20 13:31:48 +0100

    playing with feh   fbi   fim

--- gpsmap.py
+++ gpsmap.py
@@ -8,17 +8,19 @@ from staticmap import StaticMap, CircleMarker, Line
 from IPython.core.display import Image, display
 import numpy
 
+# feh --reload 0.3 map.png
+
 zoom=15
 
-m1 = StaticMap(600,400, url_template='http://localhost:8900/{z}/{x}/{y}.png',fixzoom=zoom)
-mam=[]
-maxmarkers=20
+m1 = StaticMap(650,350, url_template='http://localhost:8900/{z}/{x}/{y}.png',fixzoom=zoom)
+#mam=[]
+maxmarkers=25
 
 redraw=1
 #while True:
 
 #    print( xbin[ix], ybin[iy] )
-XCoor,YCoor=(  14.238+0.003*random.random(), 49.864 +0.003*random.random()  )
+XCoor,YCoor=(  0+0.003*random.random(), 0+0.003*random.random()  )
 
 
 
@@ -30,49 +32,74 @@ for line in task.stdout:
     redraw=0
     if (lin.find('GPGSA')>0):
         if (float(lin.split(',')[2])>1):
-            fix=''#print( 'fix' )
-            if ( lin.find('GPGLL')>0):
-                print('POSITION:')
+            fix='+' #print( 'fix' )
+            #if ( lin.find('GPGLL')>0):
+            #    print('POSITION:')
         else:
             fix=' NOFIX'
-#    if (lin.find('GPGGA')>0):
-#        tim=lin.split(',')[1]
-#        print( fix, tim[0:2]+':'+tim[2:4]+':'+tim[4:8],' UTC' )#df=pd.concat(
-    if (lin.find('GPGLL')>0):
-        redraw=1
-        tim=lin.split(',')[5]
-        print( fix, tim[0:2]+':'+tim[2:4]+':'+tim[4:8],' UTC' )#df=pd.concat(
-        YCoor,XCoor=( lin.split(',')[1], lin.split(',')[3] )
+    #print(fix)        
+    if (lin.find('GPGGA')>0):
+        tim=lin.split(',')[1]
+        YCooS,XCooS=( lin.split(',')[2], lin.split(',')[4] )
+        if (len(XCooS)<4): XCooS="0"
+        if (len(YCooS)<4): YCooS="0"
+        XCoor,YCoor=(0 +0.004*random.random(),0+0.004*random.random())
         ###########   case FIX IS OK ###########
-        if (fix==""):
-            XCoor1=floor(float(XCoor)/100.)
-            XCoor=XCoor1+(float(XCoor)-XCoor1*100)/60.
+        if (fix=="+"):
+            XCoor1=floor(float(XCooS)/100.)
+            XCoor=XCoor1+(float(XCooS)-XCoor1*100)/60.
 
-            YCoor1=floor(float(YCoor)/100.)
-            YCoor=YCoor1+(float(YCoor)-YCoor1*100)/60.
-
-            if ( lin.split(',')[1]=="S"):
+            YCoor1=floor(float(YCooS)/100.)
+            YCoor=YCoor1+(float(YCooS)-YCoor1*100)/60.
+            if ( lin.split(',')[3]=="S"):
                 YCoor=-YCoor
-                if ( lin.split(',')[1]=="E"):
-                    XCoor=-XCoor
-        else:
-            XCoor,YCoor=(  14.238+0.003*random.random(), 49.864 +0.003*random.random()  )
-        print(lin,' pos:', XCoor,YCoor )
+            if ( lin.split(',')[5]=="W"):
+                XCoor=-XCoor
+
+        print( fix, tim[0:2]+':'+tim[2:4]+':'+tim[4:8],'UTC {:6.5f} {:6.5f}'.format( XCoor,YCoor ) )
+        redraw=1
+        
+    # if (lin.find('GPGLL')>0):
+    #     redraw=1
+    #     tim=lin.split(',')[5]
+    #     print( fix, tim[0:2]+':'+tim[2:4]+':'+tim[4:8],' UTC' )#df=pd.concat(
+    #     YCoor,XCoor=( lin.split(',')[1], lin.split(',')[3] )
+    #     ###########   case FIX IS OK ###########
+    #     if (fix==""):
+    #         XCoor1=floor(float(XCoor)/100.)
+    #         XCoor=XCoor1+(float(XCoor)-XCoor1*100)/60.
+
+    #         YCoor1=floor(float(YCoor)/100.)
+    #         YCoor=YCoor1+(float(YCoor)-YCoor1*100)/60.
+
+    #         if ( lin.split(',')[1]=="S"):
+    #             YCoor=-YCoor
+    #         if ( lin.split(',')[3]=="W"):
+    #             XCoor=-XCoor
+    #     else:
+    #         XCoor,YCoor=(  14.238+0.003*random.random(), 49.864 +0.003*random.random()  )
+    #     print(lin,' pos:', XCoor,YCoor )
 
     if (redraw==1):
-        mam.append( CircleMarker( (XCoor,YCoor),  'red', 5) )
-        m1.add_marker(mam[-1], maxmarkers=maxmarkers )
+        mmaxm=maxmarkers
+        if (abs(XCoor)<0.005 and abs(YCoor)<0.005):
+            maxmarkers=2
+        color='black'
+        if fix=="+": color='red'
+        mam= CircleMarker( (XCoor,YCoor), color, 5) 
+        m1.add_marker(mam, maxmarkers=maxmarkers )
         image=m1.render()
-        image=image.resize( (1200,800) )
+        image=image.resize( (1300,700) )
         #if yrng%10:
         image.save('map.png')
         ##display( image )
         ##image.show()
         time.sleep(0.1)
-        mam.append( CircleMarker( (XCoor,YCoor),  'orange', 5) )
-        m1.add_marker( mam[-1], maxmarkers=maxmarkers )
-        print(len(mam))
+        mam= CircleMarker( (XCoor,YCoor),  'orange', 5) 
+        m1.add_marker( mam, maxmarkers=maxmarkers )
+#        print(len(mam))
         redraw=0
+        maxmarkers=mmaxm
 
 #    else:
 #        print("NOT",lin)
